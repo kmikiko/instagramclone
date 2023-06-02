575ebfd70d940fa75b6a3b51874c1e0e2f2e2233
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-02 09:55:32 +0900

    余分なファイルの生成を制限

--- config/application.rb
+++ config/application.rb
@@ -11,3 +11,16 @@ module InstagramCloneApp
     config.active_record.default_timezone = :local
   end
 end
+
+module DiveIntoWork
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.load_defaults 5.1
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+  end
+end
