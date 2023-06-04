6c488ae27650f2befc1d819dbb70767c30b2fd87
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-04 11:16:53 +0900

    userのシードデータを作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,11 @@ class FavoritesController < ApplicationController
     redirect_to posts_path, notice: "#{favorite.post.user.name}さんの投稿をお気に入り登録しました"
   end
 
+  def index
+    @user = current_user
+    @favorites = Favorite.where(user_id: @user.id).all
+  end
+
   def destroy
     favorite = current_user.favorites.find_by(id: params[:favorite_id]).destroy
     redirect_to posts_path, notice: "#{favorite.post.user.name}さんの投稿をお気に入り解除しました"
